01701f96be7fbfcecbd178410bf9dcc0f97845e1
Chanon Yaklai <taforyou@Chanons-MacBook-Pro.local>
2019-03-25 20:52:57 +0700

    try another fetch approch

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 Future<Post> fetchPost() async {
-  final response =
-      await http.get('https://horizon-testnet.stellar.org/accounts/GDJH3EQLDSLQRJQKLCMTMCNEPEU6EGZ5YIQGOQWUAMJ6RRBZ3CLKTLNY');
+  final response = await http.get(
+      'https://horizon-testnet.stellar.org/accounts/GDJH3EQLDSLQRJQKLCMTMCNEPEU6EGZ5YIQGOQWUAMJ6RRBZ3CLKTLNY');
 
   if (response.statusCode == 200) {
     // If the call to the server was successful, parse the JSON
@@ -17,8 +17,73 @@ Future<Post> fetchPost() async {
   }
 }
 
+Future<List<Article>> fetchPostNews() async {
+  String link =
+      "https://newsapi.org/v2/top-headlines?country=th&apiKey=a1cc62c2b46541ecb4726f3fefed8b2a";
+  var response = await http
+      .get(Uri.encodeFull(link), headers: {"Accept": "application/json"});
+      print(response.body);
+
+  if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+      var rest = data["articles"] as List;
+      print(rest); 
+      return rest.map<Article>((json) => Article.fromJson(json)).toList();
+  } else {
+    // If that call was not successful, throw an error.
+    throw Exception('Failed to load post');
+  }
+}
+
+class Article {
+  Source source;
+  String author;
+  String title;
+  String description;
+  String url;
+  String urlToImage;
+  String publishedAt;
+  String content;
+
+  Article(
+      {this.source,
+      this.author,
+      this.title,
+      this.description,
+      this.url,
+      this.urlToImage,
+      this.publishedAt,
+      this.content});
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+        source: Source.fromJson(json["source"]),
+        author: json["author"],
+        title: json["title"],
+        description: json["description"],
+        url: json["url"],
+        urlToImage: json["urlToImage"],
+        publishedAt: json["publishedAt"],
+        content: json["content"]);
+  }
+}
+
+class Source {
+  String id;
+  String name;
+
+  Source({this.id, this.name});
+
+  factory Source.fromJson(Map<String, dynamic> json) {
+    return Source(
+      id: json["id"] as String,
+      name: json["name"] as String,
+    );
+  }
+}
+
 class Post {
-  final List<String> balances;
+  final balances;
   //final int userId;
   //final int id;
   //final String title;
@@ -33,12 +98,14 @@ class Post {
   }
 }
 
-void main() => runApp(MyApp(post: fetchPost()));
+void main() => runApp(MyApp(post: fetchPost(),list: fetchPostNews()));
 
+// คือถ้าเป็น Stateful จะได้จัดการ State ได้
 class MyApp extends StatelessWidget {
   final Future<Post> post;
+  final Future<List<Article>> list;
 
-  MyApp({Key key, this.post}) : super(key: key);
+  MyApp({Key key, this.post, this.list}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -52,12 +119,12 @@ class MyApp extends StatelessWidget {
           title: Text('Fetch Data Example'),
         ),
         body: Center(
-          child: FutureBuilder<Post>(
-            future: post,
+          child: FutureBuilder<List<Article>>(
+            future: list,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                //print(snapshot.data.balances);
-                return Text(snapshot.data.balances.toString());
+                print(snapshot.data[0].source.name);
+                  return Text("Hey");
               } else if (snapshot.hasError) {
                 return Text("${snapshot.error}");
               }
